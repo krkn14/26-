50830c9c561d8bc5c7ef61675f27edbb8d403bb6
krkn14 <91778252+krkn14@users.noreply.github.com>
2021-10-25 14:26:18 +0300

    Add files via upload

---
+++ пр26кркн1/пр26кркн/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace пр26кркн
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        point point1;//Описали объект точка как глобальный объект
+        circle circle1;//Описали объект окружность как глобальный объект
+        ellipse ellipse1;//Описали объект эллипс как глобальный объект
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            //Создаем объект
+            point1 = new point(50, 50);
+            circle1 = new circle(150, 150, 50, Brushes.Aquamarine, Brushes.Black);
+            ellipse1 = new ellipse(200, 200, 50, Brushes.Cyan, Brushes.DarkGray);
+        }
+        //Задаем новые координаты точки
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            point1.MoveXY(Convert.ToInt32(coordX.Text), Convert.ToInt32(coordY.Text));
+        }
+        //Задаем новые координаты окружности
+        private void okr_Click(object sender, RoutedEventArgs e)
+        {
+            circle1.MoveXY(Convert.ToInt32(coordX.Text), Convert.ToInt32(coordY.Text));
+        }
+        //Задаем новые координаты эллипса
+        private void ell_Click(object sender, RoutedEventArgs e)
+        {
+            ellipse1.MoveXY(Convert.ToInt32(coordX.Text), Convert.ToInt32(coordY.Text));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ellipse1.MoveXY(ellipse1.X, ellipse1.Y - 5);
+        }
+
+        private void left_Click(object sender, RoutedEventArgs e)
+        {
+            ellipse1.MoveXY(ellipse1.X - 5, ellipse1.Y);
+        }
+
+        private void down_Click(object sender, RoutedEventArgs e)
+        {
+            ellipse1.MoveXY(ellipse1.X, ellipse1.Y + 5);
+        }
+
+        private void right_Click(object sender, RoutedEventArgs e)
+        {
+            ellipse1.MoveXY(ellipse1.X + 5, ellipse1.Y);
+        }
+    }
+}
+
---
+++ пр26кркн1/пр26кркн/point.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace пр26кркн
+{
+    class Location
+    {
+        protected int x, y;//
+        public int X//
+        {
+            get { return x; }
+        }
+        public int Y//
+        {
+            get { return y; }
+        }
+
+        //
+        public Location() { x = 0; y = 0; }
+        //
+        public Location(int initx, int inity) { x = initx; y = inity; }
+
+        //
+        public void MoveXY(int newx, int newy)
+        {
+            x = newx;
+            y = newy;
+        }
+    }
+    class point : Location
+    {
+        protected Brush fill;//
+        Rectangle rect;//
+        public Brush Fill
+        {
+            get { return fill; }
+            set { fill = value; rect.Fill = fill; rect.Stroke = fill; }
+        }
+        public Rectangle Figure//
+        { get { return rect; } }
+
+        //
+        public point()
+        {
+            //x=0;y=0;//
+            fill = Brushes.Black;//
+            rect = new Rectangle();
+            rect.Height = 3;
+            rect.Width = 3;
+            rect.Margin = new Thickness(x, y, 0, 0);
+        }
+
+        //
+        public point(int initX, int initY) : base(initX, initY)
+        {
+            //x=initX;y=initY;//
+            fill = Brushes.Black;//
+            rect = new Rectangle();
+            rect.Fill = fill;
+            rect.Stroke = fill;
+            rect.Height = 3;
+            rect.Width = 3;
+            rect.Margin = new Thickness(x, y, 0, 0);
+        }
+        public point(int initX, int initY, Brush initFill) : base(initX, initY)
+        {
+            //x=initX;y=initY;//
+            fill = initFill;//
+            rect = new Rectangle();
+            rect.Fill = fill;
+            rect.Stroke = fill;
+            rect.Height = 3;
+            rect.Width = 3;
+            rect.Margin = new Thickness(x, y, 0, 0);
+            //Canvas.SetLeft(rect,x);
+            //Canvas.SetTop(rect,y);
+        }
+
+        //
+        public void Show()
+        {
+            rect.Visibility = Visibility.Visible;
+        }
+        //
+        public void Hide()
+        {
+            rect.Visibility = Visibility.Hidden;
+        }
+
+        //
+        public new void MoveXY(int newx, int newy)
+        {
+            x = newx; y = newy;//
+            //base.MoveXY(newx,newy);
+            rect.Margin = new Thickness(x, y, 0, 0);
+            //Canvas.SetLeft(rect, x);
+        }   //Canvas.SetTop(rect, y);
+    }
+
+    class circle : point
+    {
+        protected int radius;
+        Brush stroke;//
+        Ellipse elips;//
+        public new Brush Fill//
+        {
+            get { return fill; }
+            set { fill = value; elips.Fill = fill; elips.Stroke = fill; }
+        }
+        public new Ellipse Figure//
+        { get { return elips; } }
+
+        //
+        public circle()
+        {
+            //x=0; y=0;//
+            radius = 0;
+            //fill=Brushes.Black;//
+            elips = new Ellipse();
+            elips.Height = radius * 2;
+            elips.Width = radius * 2;
+            elips.Margin = new Thickness(x - radius, y - radius, 0, 0);
+        }
+        public circle(int initX, int initY) : base(initX, initY)
+        {
+
+        }
+
+        //
+        public circle(int initX, int initY, int initR) : base(initX, initY)
+        {
+            //x=initX; y=initY;//
+            radius = initR;
+            //fill=Brushes.Black;//
+            elips = new Ellipse();
+            elips.Fill = fill;
+            elips.Stroke = stroke = Brushes.Black;
+            elips.Height = radius * 2;
+            elips.Width = radius * 2;
+            elips.Margin = new Thickness(x - radius, y - radius, 0, 0);
+        }
+
+        public circle(int initX, int initY, int initR, Brush initFill, Brush initStroke) :base(initX, initY, initFill)
+        {
+            //x=initX; y=initY;//
+            radius = initR;
+            //fill=initFill;//
+            elips = new Ellipse();
+            elips.Fill = fill;
+            elips.Stroke = stroke = initStroke;
+            elips.Height = radius * 2;
+            elips.Width = radius * 2;
+            elips.Margin = new Thickness(x - radius, y - radius, 0, 0);
+            //Canvas.SetLeft(rect, x);
+        }   //Canvas.SetTop(rect, y);
+
+        //
+        public new void Show()
+        {
+            elips.Visibility = Visibility.Visible;
+        }
+
+        //
+        public new void Hide()
+        {
+            elips.Visibility = Visibility.Hidden;
+        }
+
+        //
+        public new void MoveXY(int newx, int newy)
+        {
+            x = newx; y = newy;//
+            elips.Margin = new Thickness(x - radius, y - radius, 0, 0);
+            //Canvas.SetLeft(recr,x);
+            //Canvas.SetTop(rect, y);
+        }
+    }
+
+    class ellipse : circle
+    {
+        int radius;
+        Brush stroke;//
+        Ellipse elipse;//
+        public new Brush Fill//
+        {
+            get { return fill; }
+            set { fill = value; elipse.Fill = fill; elipse.Stroke = fill; }
+        }
+        public new Ellipse Figure//
+        { get { return elipse; } }
+        //
+        public ellipse()
+        {
+            //x=0;y=0;//
+            radius = 0;
+            //fill=Brushes.Black;//
+            elipse = new Ellipse();
+            elipse.Height = radius * 2;
+            elipse.Width = radius * 4;
+            elipse.Margin = new Thickness(x - radius, y - radius, 0, 0);
+        }
+        //
+        public ellipse(int initX,int initY,int initR) : base(initX, initY)
+        {
+            //x=initX;y=initY;//
+            radius = initR;
+            //fill=Brushes.Black;//
+            elipse = new Ellipse();
+            elipse.Fill = fill;
+            elipse.Stroke = stroke = Brushes.Black;
+        }
+        public ellipse(int initX,int initY,int initR,Brush initFill,Brush initStroke) : base(initX, initY)
+        {
+            radius = initR;
+            elipse = new Ellipse();
+            elipse.Fill = fill;
+            elipse.Stroke = stroke = initStroke;
+            elipse.Height = radius * 2;
+            elipse.Width = radius * 4;
+            elipse.Margin = new Thickness(x - radius, y - radius, 0, 0);
+        }
+        //
+        public new void Show()
+        {
+            elipse.Visibility = Visibility.Visible;
+        }
+        //
+        public new void MoveXY(int newx, int newy)
+        {
+            x = newx;y = newy;//
+            elipse.Margin = new Thickness(x - radius, y - radius, 0, 0);
+        }
+        //
+        public new void Hide()
+        {
+            elipse.Visibility = Visibility.Hidden;
+        }
+    }
+
+}
